a8727e62505c0c395f4bfecc8ae6d9401eb9190d
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-05-01 23:10:41 +0000

    Use PrimeNG Theme Aura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [ ButtonModule, RouterOutlet ],
+  template: `
+    <p>Congratulations! Your app {{ title }} is running. 🎉</p>
+    <p-button label="Check" (click)="onClick()" />
+    <router-outlet />
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'up-fullstack-fe';
+
+  onClick() {
+    console.log('Button clicked!');
+  }
+
 }
