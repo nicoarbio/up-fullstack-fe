6341521d6c55e24bc1b3eb25b724f80fb31982ce
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-05-06 23:33:13 +0000

    Refactor main AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,9 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ ButtonModule, RouterOutlet ],
-  template: `
-    <p>Congratulations! Your app {{ title }} is running. 🎉</p>
-    <p-button label="Check" (click)="onClick()" />
-    <router-outlet />
-  `,
-  styleUrl: './app.component.scss'
+  imports: [ RouterOutlet ],
+  template: `<router-outlet />`
 })
-export class AppComponent {
-  title = 'up-fullstack-fe';
-
-  onClick() {
-    console.log('Button clicked!');
-  }
-
-}
+export class AppComponent {}
