280bfc3cf9ffe223ebdf26bdf88a0859d2fa113d
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-05-06 23:34:28 +0000

    Configure routes using created Components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { BookingsComponent } from './pages/bookings/bookings.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { OrderComponent } from './pages/order/order.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'bookings', pathMatch: 'full' },
+  { path: 'bookings', component: BookingsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'order', component: OrderComponent },
+  { path: '**', redirectTo: 'bookings' } // Fallback
+];
