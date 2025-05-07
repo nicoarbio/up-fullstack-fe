3d6cce10fa1f609caafefd0dd721f15227f96da6
Nicolás G. Arbio <nicolas.arbio@despegar.com>
2025-05-07 20:06:05 -0300

    Add CanActivateFn authGuard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+
+  if (!auth.isLoggedIn()) {
+    router.navigate(['login'], {
+      queryParams: { returnUrl: state.url }
+    });
+    return false;
+  }
+
+  return true;
+};
