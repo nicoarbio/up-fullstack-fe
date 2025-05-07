d7ce4cbc15d7548882a342b75122fc4b63da181f
Nicolás G. Arbio <nicolas.arbio@despegar.com>
2025-05-07 20:07:25 -0300

    LoginComponent progress

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  private static readonly DEFAULT_RETURN_URL: string = '/profile';
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private auth: AuthService
+  ) {}
+
+  public successfulLogin(): void {
+    this.route.queryParams.subscribe(params => {
+      const returnUrl = params['returnUrl'] || LoginComponent.DEFAULT_RETURN_URL;
+      this.router.navigateByUrl(returnUrl);
+    });
+  }
 
 }
