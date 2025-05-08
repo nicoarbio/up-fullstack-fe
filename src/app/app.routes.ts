54e9eb29b2ba0ebe547422eede477b5df010e2c7
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-05-08 02:36:18 +0000

    Use dir aliases

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { BookingsComponent } from './pages/bookings/bookings.component';
-import { CartComponent } from './pages/cart/cart.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { OrderComponent } from './pages/order/order.component';
-import { authGuard } from './guards/auth.guard';
+import { LoginComponent } from '@pages/login/login.component';
+import { BookingsComponent } from '@pages/bookings/bookings.component';
+import { CartComponent } from '@pages/cart/cart.component';
+import { ProfileComponent } from '@pages/profile/profile.component';
+import { OrderComponent } from '@pages/order/order.component';
+import { authGuard } from '@guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'bookings', component: BookingsComponent },
