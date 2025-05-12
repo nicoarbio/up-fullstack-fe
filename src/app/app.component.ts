3f388218e76abdd9497e073f2e0c1c2f268505f2
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-05-12 03:39:19 +0000

    Update AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '@layouts/header/header.component';
+import { FooterComponent } from '@layouts/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet ],
-  template: `<router-outlet />`
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent
+  ],
+  template: `
+    <header>
+        <app-header [isMobile]="isMobile"></app-header>
+    </header>
+    <main>
+      <router-outlet/>
+    </main>
+    <footer>
+        <app-footer [isMobile]="isMobile"></app-footer>
+    </footer>
+  `,
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  isMobile = window.innerWidth < 768;
+
+  ngOnInit() {
+    window.addEventListener('resize', () => {
+      this.isMobile = window.innerWidth < 768;
+    });
+  }
+
+}
