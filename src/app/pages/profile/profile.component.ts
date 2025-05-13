34b5ad901bb1b84fa8c1b306897d27d37e48ec3d
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-05-13 03:00:17 +0000

    Update ProfileComponent with LogOut button

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Button } from 'primeng/button';
+import { NgIf } from '@angular/common';
+import { AuthService } from '@services/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
-  template: `<h1>Desarrollo de componente de perfil en proceso</h1>`,
+  imports: [
+    Button,
+    NgIf,
+    RouterLink
+  ],
+  template: `
+    <h1>Desarrollo de componente de perfil en proceso</h1>
+    <p-button *ngIf="showLogOutButton" label="Cerrar sesión" (click)="logout()" routerLink="/bookings"></p-button>
+  `,
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  showLogOutButton = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.loginEvent.subscribe(isLoggedIn => {
+      this.showLogOutButton = isLoggedIn;
+    });
+    this.showLogOutButton = this.authService.isLoggedIn();
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.showLogOutButton = false;
+  }
 
 }
