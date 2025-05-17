5610bd1a4c028dd7aedaedae56de40f4d96b1052
Nicolás G. Arbio <nicolasarbio@outlook.com>
2025-05-17 23:13:15 +0000

    Global Luxon configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Settings } from 'luxon';
+
+Settings.defaultLocale = 'es-AR';
+Settings.defaultZone = 'America/Argentina/Buenos_Aires';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
